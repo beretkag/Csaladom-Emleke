fb9fe481209e3efa66518bb7bb6d9146a54c19c8
Adesz05 <kerekadamtamas@turr.hu>
2023-02-10 08:07:46 +0100

    pull

--- Csaladom-Emleke/src/components/howtostart.vue
+++ Csaladom-Emleke/src/components/howtostart.vue
@@ -1,16 +1,7 @@
 <template>
-  <p>
-    Hogyan kezdj neki
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quidem illum animi unde sunt laborum voluptate quam repellat eum ipsam praesentium labore, eligendi perspiciatis rerum earum fuga commodi. Nemo, perferendis blanditiis!
-        Rerum voluptatem rem possimus labore voluptatum debitis, nemo cum sapiente deleniti eos fugit quam nostrum laborum. Voluptatum, cupiditate facilis quo dicta optio accusantium itaque, fugit suscipit deleniti odio, maxime dignissimos.
-        Reprehenderit esse modi repudiandae soluta culpa animi, vero harum fugiat rem alias debitis quod voluptatem accusamus, optio nihil officiis magnam. A veniam facilis in omnis, voluptatum amet. Soluta, suscipit debitis.
-        Saepe placeat omnis minus dignissimos atque officia ex? Itaque quaerat error aliquid aliquam obcaecati corrupti accusantium, vel earum expedita nesciunt perferendis, asperiores mollitia harum, hic ad? Ab iste debitis doloribus.
-        Tempore aliquam dolore eos. Aliquid natus suscipit magnam odio corporis optio nostrum porro ad quos delectus eligendi molestias, doloribus impedit atque deserunt, fugit illum? Expedita illo sunt ea harum rem?
-        Culpa, asperiores? Laboriosam sapiente molestiae maxime praesentium dolore doloribus commodi. Molestiae, nulla quod! Quibusdam neque accusantium fuga! Neque optio veniam quis provident, voluptates molestiae recusandae sequi vitae. Blanditiis, error ex!
-        Minus illo sapiente eum deleniti ipsum consequuntur tempore quam perferendis id pariatur, hic ad facilis quas voluptatum cumque! Doloremque placeat incidunt eveniet minus aperiam perspiciatis repudiandae repellendus similique quas praesentium?
-        Iusto debitis ab eaque, odit dolores hic dolorum nisi consequuntur tempora nihil cumque aut reiciendis veritatis magni aspernatur qui natus non est vitae tenetur? Atque nobis ullam quisquam aliquid provident!
-        Molestiae, minus? Earum quia culpa ab enim assumenda quas vel quisquam veniam inventore odio blanditiis quae omnis, reprehenderit et? Modi sed non error sequi cum delectus quis neque voluptates illum.
-        Facilis quibusdam tenetur eligendi enim consequatur veniam cumque nesciunt cupiditate voluptates recusandae, distinctio vel vitae necessitatibus atque architecto quos ratione neque voluptatem! Ad, vero magnam. Mollitia non earum officiis corrupti.
+  <p class="m-0 p-0">
+    <h1>Hogyan kezdj neki</h1>
+
   </p>
 </template>
 
@@ -19,9 +10,9 @@
 
 <style scoped>
 p{
-    margin: 2%;
+    margin: 0;
     border: 1px solid black;
     color: black;
-    padding: 2%;
+    padding: 0;
 }
 </style>
