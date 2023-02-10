60c3449334bdde161bc1f93f88f6722c7a3c09aa
Adesz05 <kerekadamtamas@turr.hu>
2023-02-10 09:39:52 +0100

    alakul

--- Csaladom-Emleke/src/components/howtostart.vue
+++ Csaladom-Emleke/src/components/howtostart.vue
@@ -1,18 +1,32 @@
 <template>
-  <p class="m-0 p-0">
+  <p class="cim m-0 p-0">
     <h1>Hogyan kezdj neki</h1>
-
   </p>
+  <div class=" mb-3 row m-0">
+    <span class="col-1"></span>
+    <div class="col-5 p-0"><img src="../assets/regisztracio.png" alt=""></div>
+    <p class="p-0 col">Az első lépés a regisztráció! Regisztráció során ha megadja a szülei nevét akkor ők már rögtön bekerülnek a családfájába.</p>
+  </div>
+  <div class="mb-3 row m-0">
+    <p class="p-0 col-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, aperiam reprehenderit. Quas, dolores a necessitatibus itaque dolore eligendi sunt ipsam reiciendis quisquam, beatae impedit totam quaerat eveniet inventore ad nemo.</p>
+    <div class="p-0 col"><img src="../assets/harmadik.jpg" alt=""></div>
+  </div>
+  <div class="mb-3 row m-0">
+    <div class="p-0 col-8"><img src="../assets/negyedik.jpg" alt=""></div>
+    <p class="p-0 col">Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempora est quam culpa eum? Iure culpa fuga nihil ad ea consectetur quo voluptatem reiciendis doloribus eius quam, recusandae quia minus quisquam.</p>
+  </div>
 </template>
 
 <script>
 </script>
 
 <style scoped>
-p{
+.cim{
     margin: 0;
     border: 1px solid black;
     color: black;
     padding: 0;
 }
+
+
 </style>
