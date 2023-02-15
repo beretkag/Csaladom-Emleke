884155c35c10d9b2134a6906918cfab5116b422a
kkrisztian <kerteszkrisztian@turr.hu>
2023-02-15 13:52:30 +0100

    usermanagment oldal

--- Csaladom-Emleke/src/components/family/usermanagment.vue
+++ Csaladom-Emleke/src/components/family/usermanagment.vue
@@ -1,11 +1,45 @@
 <template>
-    <h1>
-        Felhasználókezelés
-    </h1>
+    
+    <table v-for="felhasznalo in felhasznalok">
+        <tr>
+            <td>
+                {{felhasznalo}}
+            </td>
+            <td>
+                Módosítás
+            </td>
+            <td>
+                Törlés
+            </td>
+        </tr>
+    </table>
+
    </template>
    
    <script>
+
+import axios from "axios";
+
+export default{
+data(){
+    return {
+        felhasznalok: []
+           }
+      },
    
+created(){
+    axios.get(this.$store.baseURL + "/felhasznalok").then(res => {
+        let data = res.data;
+            this.felhasznalok = res.data 
+    })
+    .catch(err => {
+        console.log(err);
+    })
+    ;
+
+    }
+}
+
    </script>
    
    <style>
