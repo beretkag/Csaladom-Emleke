55b590c531843107c961e676dd0a90d3355314f6
kkrisztian <kerteszkrisztian@turr.hu>
2023-02-16 09:25:59 +0100

    felhasználókezelés oldalon szépen kiírja az adatokat

--- Csaladom-Emleke/src/components/family/usermanagment.vue
+++ Csaladom-Emleke/src/components/family/usermanagment.vue
@@ -1,18 +1,26 @@
 <template>
-    
-    <table v-for="felhasznalo in felhasznalok">
-        <tr>
-            <td>
-                {{felhasznalo}}
-            </td>
-            <td>
-                Módosítás
-            </td>
-            <td>
-                Törlés
-            </td>
-        </tr>
-    </table>
+    <table class="table table-hover table-dark">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Családnév</th>
+      <th scope="col">Keresztnév</th>
+      <th scope="col">Email-cím</th>
+      <th> Módosítás </th>
+      <th> Törlés </th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr v-for="felhasznalo, index in felhasznalok">
+      <th scope="row" class="align-middle"> {{ index + 1}}</th>
+      <td class="align-middle">{{felhasznalo.Nev.split(' ')[1]}}</td>
+      <td class="align-middle">{{felhasznalo.Nev.split(' ')[0]}}</td>
+      <td class="align-middle">{{ felhasznalo.email }}</td>
+      <td class="align-middle"> <button class="btn btn-dark btn-lg "> <i class="bi bi-pencil-square" ></i>  </button></td>
+      <td class="align-middle"> <button class="btn btn-dark btn-lg" @click="DeleteUser(felhasznalo.id)"> <i class="bi bi-person-x"></i></button>  </td>
+    </tr>
+  </tbody>
+</table>
 
    </template>
    
@@ -28,18 +36,19 @@ data(){
       },
    
 created(){
-    axios.get(this.$store.baseURL + "/felhasznalok/nev").then(res => {
+    axios.get(this.$store.getters.baseURL + "/felhasznalok").then(res => {
         let data = res.data;
             this.felhasznalok = res.data 
     })
     .catch(err => {
         console.log(err);
     });
-    }
+    },
+    
 }
+
 
    </script>
    
    <style>
-   
    </style>
