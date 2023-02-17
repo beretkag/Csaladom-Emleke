dfe37996e0c72317e347c0eba25ba5bb7b335630
beretkag <beretkag@gmail.com>
2023-02-17 14:10:30 +0100

    sidebar

--- Csaladom-Emleke/src/views/csalad.vue
+++ Csaladom-Emleke/src/views/csalad.vue
@@ -1,94 +1,66 @@
 <template>
     <FamilyMenu />
     <div id="app">
-        <FamilyTree v-if="Csaladfa" ref="tree" />
-        <Settings v-if="Beallitasok"/>
-        <Contacts v-if="Elerhetosegek"/>
-        <UserManagment v-if="Felhasznalokezeles"/>
+        <i class="bi bi-menu-up btn btn-primary rounded-circle m-3" id="menuBtn" data-bs-toggle="offcanvas" data-bs-target="#offcanvasScrolling" aria-controls="offcanvasScrolling"></i>
+        <FamilyTree ref="tree" />
     </div>
 </template>
 
 <script>
-    import axios from 'axios';
-    import FamilyTree from '../components/family/FamilyTree.vue';
-    import FamilyMenu from '../components/family/familymenu.vue';
-    import Settings from '../components/family/settings.vue';
-    import Contacts from '../components/family/contacts.vue';
-    import UserManagment from '../components/family/usermanagment.vue'
+import axios from 'axios';
+import FamilyTree from '../components/family/FamilyTree.vue';
+import FamilyMenu from '../components/family/familymenu.vue';
 
-    export default{
-        components:{
-    FamilyMenu,
-    FamilyTree,
-    Settings,
-    Contacts,
-    UserManagment
-},
-data(){
-      return {
-        Csaladfa: true,
-        Beallitasok: false,
-        Elerhetosegek: false,
-        Felhasznalokezeles: false
-      }
+export default{
+    components:{
+        FamilyMenu,
+        FamilyTree,
+        axios
     },
-    methods:{
-        CurrentPage(szam){
-        if (szam == 4) {
-            this.Elerhetosegek = true
-            this.Csaladfa = false,
-            this.Beallitasok = false,
-            this.Felhasznalokezeles = false
+    data(){
+        return {
         }
-        else if(szam == 3){
-            this.Beallitasok = false, 
-            this.Csaladfa = false,
-            this.Elerhetosegek = false,
-            this.Felhasznalokezeles = true
-        }
-        else if(szam == 2){
-            this.Beallitasok = true, 
-            this.Csaladfa = false,
-            this.Elerhetosegek = false,
-            this.Felhasznalokezeles = false
-        }
-        else if(szam == 1){
-            this.Csaladfa= true,
-            this.Beallitasok = false, 
-            this.Elerhetosegek = false,
-            this.Felhasznalokezeles = false
-        }
-      }
-    },
-
-
-        created(){
-            //családfa betöltése------------------------------------------------------------------------------------>
-            axios.get(this.$store.getters.baseURL + "/csaladfak/felhasznaloID/" + this.$store.getters.loggedUser.ID)
-            .then(res => {
-                let user = this.$store.getters.loggedUser;
-                this.$store.commit('SetUser', Object.assign(user, {csaladfak: res.data}))
-                let fa = this.$store.getters.loggedUser.csaladfak.find(x => x.alapertelmezett == 0);
-                axios.get(this.$store.getters.baseURL + "/csaladtagok/csaladfaID/" + fa.ID)
-                .then(res => {
-                    this.$store.commit('SetMembers', res.data);
-                    this.$refs.tree.Rajzol();
-                })
-            })
-            //------------------------------------------------------------------------------------------------------>
         },
-        
-    }
+        methods:{
+
+        },
+
+
+            created(){
+                //családfa betöltése------------------------------------------------------------------------------------>
+                axios.get(this.$store.getters.baseURL + "/csaladfak/felhasznaloID/" + this.$store.getters.loggedUser.ID)
+                .then(res => {
+                    let user = this.$store.getters.loggedUser;
+                    this.$store.commit('SetUser', Object.assign(user, {csaladfak: res.data}))
+                    let fa = this.$store.getters.loggedUser.csaladfak.find(x => x.alapertelmezett == 0);
+                    axios.get(this.$store.getters.baseURL + "/csaladtagok/csaladfaID/" + fa.ID)
+                    .then(res => {
+                        this.$store.commit('SetMembers', res.data);
+                        this.$refs.tree.Rajzol();
+                    })
+                })
+                //------------------------------------------------------------------------------------------------------>
+            },
+            
+        }
 </script>
 
 <style>
+
+#menuBtn{
+    position: absolute;
+    top: 5px;
+    left: 5px;
+    z-index: 999;
+}
+
 #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    height: 80vh
+    height: 100vh
 }
 
 html, body {
