94585a89d1d23376452cd9479a9e572b0891a6e1
kkrisztian <kerteszkrisztian@turr.hu>
2023-02-17 08:38:19 +0100

    liferoad oldal/aloldalak létrehozása, felesleges dolgok törlése

---
+++ Csaladom-Emleke/src/components/family/liferoad/LifeRoadMenu.vue
@@ -0,0 +1,39 @@
+<template>
+
+    <div>
+        <liferoad v-if="VanNeki" />
+        <newliferoad v-else />
+    </div>
+
+</template>
+
+<script>
+
+import liferoad from '../liferoad/LifeRoad.vue'
+import newliferoad from '../liferoad/NewLifeRoad.vue'
+import axios from "axios";
+
+export default{
+data(){
+    return {
+        felhasznalo: []
+           }
+      },
+   
+created(){
+    
+    axios.get(this.$store.getters.baseURL + "/eletut" + id).then(res => {
+        let data = res.data;
+            this.felhasznalok = res.data 
+    })
+    .catch(err => {
+        console.log(err);
+    })
+}
+}
+
+</script>
+
+<style>
+
+</style>
