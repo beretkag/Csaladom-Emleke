de210da2f5151d21594c18f688ee8ec6bd43b406
Adesz05 <kerekadamtamas@turr.hu>
2023-02-24 12:30:53 +0100

    haladunk

--- Csaladom-Emleke/src/views/eletut.vue
+++ Csaladom-Emleke/src/views/eletut.vue
@@ -9,18 +9,23 @@
     <main>
         <div class="m-3" v-for="paragraph, index in paragraphs">
             <div class="row">
-                <h3 class="col-10" >{{ paragraph.cim }}</h3>
+                <input v-if="paragraph.edit" type="text" class="form-control col-10"  placeholder="Cím" v-model="para">
+                <h3 class="col-10" v-else >{{ paragraph.cim }}</h3>
                 <div class="col-2 d-flex flex-row-reverse">    
-                    <button class="m-1 m-lg-2 m-sm-1 btn btn-danger" @click="Torles(paragraph.ID)"><i class="bi bi-trash"></i></button>
-                    <button class="m-1 m-lg-2 m-sm-1 btn btn-warning" @click="Szerekesztes(paragraph.ID)"><i class="bi bi-pencil"></i></button>
+                    <button v-if="paragraph.edit" class="m-1 m-lg-2 m-sm-1 btn btn-danger" @click="Megse(paragraph)"><i class="bi bi-x-lg"></i></button>
+                    <button v-else class="m-1 m-lg-2 m-sm-1 btn btn-danger" @click="Torles(paragraph)"><i class="bi bi-trash"></i></button>
+                    <button v-if="paragraph.edit" class="m-1 m-lg-2 m-sm-1 btn btn-warning" @click="SzerekesztesVeglegesites(paragraph)"><i class="bi bi-check-lg"></i></button>
+                    <button v-else class="m-1 m-lg-2 m-sm-1 btn btn-warning" @click="Szerekesztes(paragraph)"><i class="bi bi-pencil"></i></button>
                 </div>
             </div>
-            <p class="m-3">{{ paragraph.szoveg }}</p>
+            <textarea v-if="paragraph.edit" class="form-control m-3" aria-label="With textarea"></textarea>
+            <p v-else class="m-3">{{ paragraph.szoveg }}</p>
             <hr>
         </div>
     </main>
 </template>
 <script>
+import { trigger } from '@vue/reactivity';
 import axios from 'axios';
 
 export default{
@@ -41,17 +46,23 @@ export default{
             axios.get(this.$store.getters.baseURL+"/eletut/csaladtagID/"+this.nodeid, {headers: {"authorization": "JWT "+this.$store.getters.Token}})
             .then(res=>{
                 this.paragraphs=res.data;
+                this.paragraphs.forEach(element => {
+                    element.edit=false;
+                });
             })
         })
         
     },
     methods:{
-        Torles(Id){
+        Torles(paragraph){
             axios.delete(this.$store.getters.baseURL+"/eletut/ID/"+Id, {headers: {"authorization": "JWT "+this.$store.getters.Token}})
             .then(res =>{
-                this.paragraphs.splice(this.paragraphs.findIndex(x=>x.ID == Id),1)
+                this.paragraphs.splice(this.paragraphs.findIndex(x=>x.ID == paragraph.ID),1)
             })
             console.log(this.paragraphs)
+        },
+        Szerekesztes(paragraph){
+            paragraph.edit=true;
         }
     }
 
