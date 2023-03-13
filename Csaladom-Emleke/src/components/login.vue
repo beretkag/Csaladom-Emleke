fdb1333819122de4ebb3290884efdbd03bf16620
beretkag <beretkag@gmail.com>
2023-03-13 12:28:28 +0100

    hibajelölések a login és regisztráció oldalon

--- Csaladom-Emleke/src/components/login.vue
+++ Csaladom-Emleke/src/components/login.vue
@@ -4,9 +4,9 @@
            Bejelentkezés
        </h1>
        <label for="email"> E-mail cím </label>
-       <input v-model="user.email" type="email" name="email" class="form-control mb-3">
+       <input v-model="user.email" type="email" name="email" class="form-control mb-3" :class="{'is-invalid' : missings.email}">
        <label for="passwd"> Jelszó: </label>
-       <input v-model="user.password" type="password" name="passwd" class="form-control">
+       <input v-model="user.password" type="password" name="passwd" class="form-control" :class="{'is-invalid' : missings.password}">
       <a id="forgotpass" data-bs-toggle="modal" data-bs-target="#exampleModal" class="mb-3"> Elfelejtette a jelszavát? </a>
        
        <button id="loginbutton" class="btn" @click="LogIn()">
@@ -26,7 +26,11 @@
     data(){
       return{
         baseURL: this.$store.getters.baseURL + "/login",
-        user: {}
+        user: {},
+        missings: {
+          email: false,
+          password: false
+        }
       }
     },
     methods:{
@@ -36,23 +40,38 @@
           email: this.user.email,
           password: `${sha256(this.user.password)}`
         }
-        axios.post(this.baseURL, data)
-        .then((res) => {
-          if (res.data.length < 1) {
-            this.$store.commit('ShowMsg', {text:"Hibás felhasználónév vagy jelszó!", type: "danger"})
-          } else {
-            //Sikeres bejelentkezés
-            sessionStorage.setItem('csaladomemleke', JSON.stringify(res.data));
-            axios.post(this.$store.getters.baseURL+ "/user/data", {token :'JWT ' + res.data})
-            .then(res =>{
-              axios.get(this.$store.getters.baseURL + "/csaladfak/felhasznaloID/" + res.data.ID, {headers: {"authorization": "JWT "+JSON.parse(sessionStorage.getItem('csaladomemleke'))}})
-              .then(results => {
-                this.$router.push(import.meta.env.BASE_URL + "csalad/" + results.data[0].ID);
+        if (this.Missings().length > 0) {
+          this.$store.commit('ShowMsg', {text:"Nem töltött ki minden adatot!", type: "danger"})
+        } else {
+          axios.post(this.baseURL, data)
+          .then((res) => {
+            if (res.data.length < 1) {
+              this.$store.commit('ShowMsg', {text:"Hibás felhasználónév vagy jelszó!", type: "danger"})
+              this.missings.password = true;
+            } else {
+              //Sikeres bejelentkezés
+              sessionStorage.setItem('csaladomemleke', JSON.stringify(res.data));
+              axios.post(this.$store.getters.baseURL+ "/user/data", {token :'JWT ' + res.data})
+              .then(res =>{
+                axios.get(this.$store.getters.baseURL + "/csaladfak/felhasznaloID/" + res.data.ID, {headers: {"authorization": "JWT "+JSON.parse(sessionStorage.getItem('csaladomemleke'))}})
+                .then(results => {
+                  this.$router.push(import.meta.env.BASE_URL + "csalad/" + results.data[0].ID);
+                })
               })
-            })
+            }
+          })
+        }
+      },
+      Missings(){
+        let required = Object.keys(this.missings);
+        let missed = [];
+        required.forEach(item => {
+          if (this.user[item] == null || this.user[item].length == 0) {
+            missed.push(item);
+            this.missings[item] = true;
           }
-        })
-        .catch((err) => console.log(err));
+        });
+        return missed;
       }
     }
 
--- Csaladom-Emleke/src/components/registration.vue
+++ Csaladom-Emleke/src/components/registration.vue
@@ -106,15 +106,18 @@ export default {
       }else{
         if (!this.newUser.email.match(/(?:[a-z0-9+!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/gi)) {
           this.$store.commit('ShowMsg', {text:"Helytelen e-mail cím formátum!", type: "danger"})
+          this.missings.email = true;
         }
         else{
           if (!this.newUser.password.match((/^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])([a-zA-Z0-9]{8,})$/))) {
             this.$store.commit('ShowMsg', {text:"A jelszó nem felel meg a követelményeknek!", type: "danger"})
+            this.missings.password = true;
           } else {
           axios.get(this.baseURL + "/" + table + "/" + field + "/" + value)
             .then((res)=>{
               if (res.data.length > 0) {
                 this.$store.commit('ShowMsg', {text:"Ezzel az e-mail címmel már regisztráltak!", type: "danger"})
+                this.missings.email = true;
               }
               else{
                 let data ={};
