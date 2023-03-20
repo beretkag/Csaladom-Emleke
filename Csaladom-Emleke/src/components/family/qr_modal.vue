282ef34a8b1970c7b52aae6de4b18b11119ed845
Adesz05 <kerekadamtamas@turr.hu>
2023-03-20 11:11:09 +0100

    QR kód modal és pull

---
+++ Csaladom-Emleke/src/components/family/qr_modal.vue
@@ -0,0 +1,33 @@
+<template>
+    <!-- Button trigger modal -->
+    <button type="button" class="btn btn-primary" >
+    </button>
+
+    <!-- Modal -->
+    <div class="modal fade" id="qrmodal" data-bs-backdrop="static" data-bs-keyboard="false">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+        <div class="modal-header">
+            <h1 class="modal-title fs-5" id="qrmodalLabel">Modal title</h1>
+            <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+            ...
+        </div>
+        <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Understood</button>
+        </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+
+<script>
+export default{
+
+}
+
+
+</script>
