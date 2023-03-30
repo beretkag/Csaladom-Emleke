90cb4bf04211bad52796a8f73085c6fda789272f
beretkag <beretkag@gmail.com>
2023-03-30 13:02:09 +0200

    adatbázis frissítés

--- Csaladom-Emleke/API/sql/127_0_0_1.sql
+++ Csaladom-Emleke/API/sql/127_0_0_1.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Gép: 127.0.0.1
--- Létrehozás ideje: 2023. Már 24. 12:29
+-- Létrehozás ideje: 2023. Már 30. 13:00
 -- Kiszolgáló verziója: 10.4.6-MariaDB
 -- PHP verzió: 7.3.8
 
@@ -36,15 +36,18 @@ CREATE TABLE `beallitasok` (
   `publikus` tinyint(1) NOT NULL,
   `darkmode` tinyint(1) NOT NULL DEFAULT 0,
   `noszin` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL,
-  `ferfiszin` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL
+  `ferfiszin` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `ferfibetuszin` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL,
+  `nobetuszin` varchar(20) COLLATE utf8_hungarian_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
 --
 -- A tábla adatainak kiíratása `beallitasok`
 --
 
-INSERT INTO `beallitasok` (`ID`, `csaladfaID`, `publikus`, `darkmode`, `noszin`, `ferfiszin`) VALUES
-(1, 5, 1, 1, '#FF8811', '#2C99EB');
+INSERT INTO `beallitasok` (`ID`, `csaladfaID`, `publikus`, `darkmode`, `noszin`, `ferfiszin`, `ferfibetuszin`, `nobetuszin`) VALUES
+(1, 5, 1, 1, '#FF8811', '#2E7DFF', '#FFFFFF', '#FFFFFF'),
+(39, 56, 0, 0, NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -63,7 +66,8 @@ CREATE TABLE `csaladfak` (
 --
 
 INSERT INTO `csaladfak` (`ID`, `felhasznaloID`, `Nev`) VALUES
-(5, 6, 'Családfa');
+(5, 6, 'Családfa'),
+(56, 20, 'Ádám Kerék');
 
 -- --------------------------------------------------------
 
@@ -94,9 +98,10 @@ CREATE TABLE `csaladtagok` (
 
 INSERT INTO `csaladtagok` (`ID`, `csaladfaID`, `belsofaID`, `profilkep`, `telefonszam`, `keresztnev`, `vezeteknev`, `gender`, `szulhely`, `szulido`, `halido`, `mid`, `fid`, `partnerek`) VALUES
 (4, 5, 'gfhd', NULL, NULL, 'Gellért', 'Beretka', 'male', NULL, '1999-08-23', NULL, '_uq02', NULL, '_b8n9'),
-(8, 5, '_uq02', NULL, NULL, 'Zsuzsanna', 'Bíró', 'female', NULL, NULL, NULL, NULL, NULL, NULL),
+(8, 5, '_uq02', NULL, '06308743854', 'Zsuzsanna', 'Bíró', 'female', NULL, NULL, NULL, NULL, NULL, NULL),
 (136, 5, '_3h88', NULL, NULL, NULL, NULL, 'male', NULL, NULL, NULL, '_b8n9', 'gfhd', NULL),
-(137, 5, '_b8n9', NULL, NULL, NULL, NULL, 'female', NULL, NULL, NULL, NULL, NULL, 'gfhd');
+(137, 5, '_b8n9', NULL, NULL, NULL, NULL, 'female', NULL, NULL, NULL, NULL, NULL, 'gfhd'),
+(147, 56, 'aaaa', NULL, NULL, 'Ádám', 'Kerék', 'male', NULL, '2003-04-26', NULL, NULL, NULL, NULL);
 
 -- --------------------------------------------------------
 
@@ -110,6 +115,13 @@ CREATE TABLE `eletut` (
   `cim` varchar(100) COLLATE utf8_hungarian_ci DEFAULT NULL,
   `szoveg` text COLLATE utf8_hungarian_ci DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
+
+--
+-- A tábla adatainak kiíratása `eletut`
+--
+
+INSERT INTO `eletut` (`ID`, `csaladtagID`, `cim`, `szoveg`) VALUES
+(15, 8, 'dsfsdf', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\nWhy do we use it?\nIt is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using \'Content here, content here\', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for \'lorem ipsum\' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).\n\n\nWhere does it come from?\nContrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of \"de Finibus Bonorum et Malorum\" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, \"Lorem ipsum dolor sit amet..\", comes from a line in section 1.10.32.\n\nThe standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from \"de Finibus Bonorum et Malorum\" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.');
 
 -- --------------------------------------------------------
 
@@ -132,7 +144,8 @@ CREATE TABLE `felhasznalok` (
 --
 
 INSERT INTO `felhasznalok` (`ID`, `Nev`, `Jelszo`, `email`, `jogosultsag`, `tiltas`, `tiltasmessage`) VALUES
-(6, 'Gellért', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin@admin.com', 2, 0, NULL);
+(6, 'Gellért', '8c6976e5b5410415bde908bd4dee15dfb167a9c873fc4bb8a81f6f2ab448a918', 'admin@admin.com', 2, 0, NULL),
+(20, 'Ádám Kerék', '6ebbeebd5817fc4b82f793231bf0de57d003f37baf7119dc3ab58bd829b792e9', 'teszt@teszt.com', 1, 0, NULL);
 
 -- --------------------------------------------------------
 
@@ -201,37 +214,37 @@ ALTER TABLE `kepek`
 -- AUTO_INCREMENT a táblához `beallitasok`
 --
 ALTER TABLE `beallitasok`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=40;
 
 --
 -- AUTO_INCREMENT a táblához `csaladfak`
 --
 ALTER TABLE `csaladfak`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=52;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=57;
 
 --
 -- AUTO_INCREMENT a táblához `csaladtagok`
 --
 ALTER TABLE `csaladtagok`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=143;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=175;
 
 --
 -- AUTO_INCREMENT a táblához `eletut`
 --
 ALTER TABLE `eletut`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
 
 --
 -- AUTO_INCREMENT a táblához `felhasznalok`
 --
 ALTER TABLE `felhasznalok`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
 
 --
 -- AUTO_INCREMENT a táblához `kepek`
 --
 ALTER TABLE `kepek`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=224;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=262;
 
 --
 -- Megkötések a kiírt táblákhoz
